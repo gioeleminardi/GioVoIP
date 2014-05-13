55364a0d9beb87567d02aa979c464ace1a9762ce
Gioele Minardi <gioelem3@gmail.com>
2014-05-13 23:48:56 +0200

    Hello World :D

---
+++ Server/server.cpp
@@ -0,0 +1,7 @@
+#include "srv_header.h"
+
+void main(){
+
+	std::cout << "Hello World!\n";
+	getchar();
+}
---
+++ Server/srv_header.h
@@ -0,0 +1,6 @@
+#define WIN32_LEAN_AND_MEAN	
+#include <Windows.h>
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
